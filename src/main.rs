6f45777facc1433a10cb8121e09f63981758e74c
Ollie Rees <ollie.rees@me.com>
2023-11-19 17:30:44 +0000

    Updating main to use cli input

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,129 @@
-use std::fs;
+use std::num::ParseIntError;
+use std::{fmt, io, fs};
+use std::io::{Read, Write};
 
 mod day_1;
 use day_1::calorie_count;
 
-mod day_2;
-use day_2::rps;
-
-mod day_3;
-use day_3::rucksack;
-
-mod day_4;
-use day_4::camp_cleanup;
-
-mod day_5;
-use day_5::supply_stacks;
-
-mod day_6;
-use day_6::tuning_trouble;
-
-mod day_7;
-use day_7::file_system;
-
-enum AnswerMode {
-    Practice,
-    Real,
+enum InputFolder {
+    Practice {year: u32, day: u8},
+    Real {year: u32, day: u8},
 }
 
-const DAY: u8 = 5;
-
-fn answer_mode_path(mode: AnswerMode) -> String {
-    match mode {
-        AnswerMode::Practice => "practice".to_owned(),
-        AnswerMode::Real => "real".to_owned(),
+impl fmt::Display for InputFolder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputFolder::Practice { year, day } => write!(f, "inputs/{year}/{day}/practice.txt"),
+            InputFolder::Real { year, day } => write!(f, "inputs/{year}/{day}/real.txt"),
+        }
     }
 }
 
-type Solution = fn(Vec<String>);
-const FUNCTIONS: [Solution; 7] = [
-    calorie_count::main,
-    rps::main,
-    rucksack::main,
-    camp_cleanup::main,
-    supply_stacks::main,
-    tuning_trouble::main,
-    file_system::main,
-];
+fn stdio_read_integer<R: Read>(mut reader: R) -> Result<u32, ParseIntError> {
+    let mut input = String::new();
+    reader.read_to_string(&mut input).unwrap();
+    input.trim().parse()
+}
 
-fn read_input(folder: AnswerMode) -> Vec<String> {
-    let filepath = format!("../inputs/{}/{DAY}.txt", answer_mode_path(folder));
-    println!("{}", filepath);
+fn get_day<R: Read>(reader: R) -> u8 {
+    print!("Please enter a day: ");
+    let _ = io::stdout().flush();
+    match stdio_read_integer(reader) {
+        Ok(n) => n as u8,
+        Err(_) => 1,
+    } 
+}
+
+fn get_year<R: Read>(reader: R) -> u32 {
+    print!("Please enter a year: ");
+    let _ = io::stdout().flush();
+    match stdio_read_integer(reader) {
+        Ok(n) => n,
+        Err(_) => 2022,
+    } 
+}
+
+fn read_problem_input_file(filepath: String) -> Vec<String> {
     let contents: String = fs::read_to_string(filepath).unwrap();
-    let contents: String = contents
-        .strip_suffix("\n")
-        .unwrap_or(contents.as_str())
-        .to_owned();
-
-    contents
-        .split("\n")
-        .map(|line: &str| line.to_owned())
-        .collect::<Vec<String>>()
-}
-
-pub fn trim_input(input_lines: Vec<String>) -> Vec<String> {
-    input_lines
-        .into_iter()
-        .map(|line| line.trim().to_owned())
-        .collect()
-}
-
-fn get_answers_for_day(folder: AnswerMode) {
-    let lines = read_input(folder);
-    FUNCTIONS.get(usize::from(DAY - 1)).unwrap()(lines);
+    contents.split("\n").map(|line| line.trim().to_owned()).collect()
 }
 
 fn main() {
-    println!("\nPRACTICE");
-    get_answers_for_day(AnswerMode::Practice);
-    println!("\nREAL");
-    get_answers_for_day(AnswerMode::Real);
+    let year: u32 = get_year(io::stdin());
+    let day: u8 = get_day(io::stdin());
+    println!("Reading answers for day {day} in {year}");
+    let practice_input = read_problem_input_file(InputFolder::Practice { year, day }.to_string());
+    let real_input = read_problem_input_file(InputFolder::Real { year, day }.to_string());
+    println!("Practice Answers: ");
+    calorie_count::main(practice_input);
+    println!("Real Answers: "); 
+    calorie_count::main(real_input);
+}
+
+#[cfg(test)] 
+mod main {
+    use std::num::ParseIntError;
+
+    use crate::{stdio_read_integer, InputFolder, get_day, get_year, read_problem_input_file};
+
+    #[test]
+    fn practice_input_folder_formats_correctly() {
+        let mock_folder = InputFolder::Practice { year: 2022, day: 1 }.to_string();
+        assert_eq!(mock_folder, "inputs/2022/1/practice.txt");
+    }
+    
+    #[test]
+    fn real_input_folder_formats_correctly() {
+        let mock_folder = InputFolder::Real { year: 2022, day: 1 }.to_string();
+        assert_eq!(mock_folder, "inputs/2022/1/real.txt");
+    }
+
+    #[test]
+    fn read_integer() {
+        let input = &b"2"[..];
+        assert_eq!(stdio_read_integer(input), Ok(2));
+        assert_eq!(get_day(input), 2);
+        assert_eq!(get_year(input), 2);
+    }
+
+    #[test]
+    fn read_integer_with_spaces() {
+        let input = &b"         2    "[..];
+        assert_eq!(stdio_read_integer(input), Ok(2));
+        assert_eq!(get_day(input), 2);
+        assert_eq!(get_year(input), 2);
+    }
+
+    #[test]
+    fn read_negative_integer() {
+        let input = &b"-2"[..];
+        let inputted_int: Result<u32, ParseIntError> = stdio_read_integer(input);
+        assert!(inputted_int.is_err());
+        assert_eq!(get_day(input), 1);
+        assert_eq!(get_year(input), 2022);
+    }
+
+    #[test]
+    fn read_integer_with_alphabetical_chars() {
+        let input = &b"abc123"[..];
+        let inputted_int: Result<u32, ParseIntError> = stdio_read_integer(input);
+        assert!(inputted_int.is_err());
+        assert_eq!(get_day(input), 1);
+        assert_eq!(get_year(input), 2022);
+    }
+
+    #[test]
+    fn read_integer_with_no_input() {
+        let input = &b""[..];
+        let inputted_int: Result<u32, ParseIntError> = stdio_read_integer(input);
+        assert!(inputted_int.is_err());
+        assert_eq!(get_day(input), 1);
+        assert_eq!(get_year(input), 2022);
+    }
+
+    #[test]
+    fn read_input() {
+        let filepath: String = "src/mocks/problem_input.txt".to_owned();
+        assert_eq!(read_problem_input_file(filepath).get(0), Some(&"1000".to_owned()));
+    }
 }
