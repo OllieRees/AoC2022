4ce8b1b89ab21a3496776adf37bc3e173ae908cf
Ollie Rees <ollie.rees@me.com>
2023-11-20 21:22:22 +0000

    Function composition and reducing scope of functions

--- src/year_2022/day_1/calorie_count.rs
+++ src/year_2022/day_1/calorie_count.rs
@@ -1,29 +1,26 @@
 use itertools::Itertools;
 
-fn sum_calories(lines: Vec<String>) -> Vec<u32> {
-    let grouped_elves: Vec<&[String]> = lines.split(|calorie: &String| calorie.eq("")).collect();
+fn group_calories(lines: Vec<String>) -> Vec<Vec<u32>> {
+    let mut calorie_groups: Vec<Vec<u32>> = Vec::new();
+    for (key, group) in &lines.into_iter().group_by(|line| !line.is_empty()) {
+        if key {
+            calorie_groups.push(group.map(|calorie| calorie.parse::<u32>().unwrap()).collect::<Vec<u32>>());
+        }
+    }
+    calorie_groups
+}
 
-    grouped_elves
-        .iter()
-        .map(|calories: &&[String]| {
-            calories
-                .into_iter()
-                .map(|calorie| {
-                    if let Ok(x) = calorie.parse::<u32>() {
-                        return x;
-                    }
-                    0
-                })
-                .sum::<u32>()
-        })
-        .collect()
+fn sum_calories(elf_groups: Vec<Vec<u32>>) -> Vec<u32> {
+    elf_groups.into_iter().map(|group : Vec<u32>| group.into_iter().sum()).collect()
+}
+
+fn largest_n_calories(total_calories: Vec<u32>, n: usize) -> Vec<u32> {
+    total_calories.into_iter().sorted_by(|a, b| Ord::cmp(&b, &a)).take(n).collect()
 }
 
 pub fn solve(lines: Vec<String>) {
-    let calorie_sum: Vec<u32> = sum_calories(lines);
-    println!("{}", &calorie_sum.iter().max().unwrap());
-    println!(
-        "{}",
-        &calorie_sum.into_iter().sorted().rev().take(3).sum::<u32>()
-    );
+    let calorie_sum: Vec<u32> = sum_calories(group_calories(lines));
+    let calorie_maximal = | n: usize | largest_n_calories(calorie_sum.clone(), n).into_iter().sum::<u32>();
+    println!("{}", calorie_maximal(1));
+    println!("{}", calorie_maximal(3));
 }
