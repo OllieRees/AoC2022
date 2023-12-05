3d72d64d4729fc000a698b3f283a8cbef0318251
Ollie Rees <ollie.rees@me.com>
2023-12-05 10:27:30 +0000

    Initial commit of files for day 5

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ fn get_module(year: u32, day: u8) -> Option<impl Fn(Vec<String>)> {
                 2 => Some(day_2::cube_conundrum::solve),
                 3 => Some(day_3::gear_ratio::solve),
                 4 => Some(day_4::scratchcards::solve),
+                5 => Some(day_5::planting_seeds::solve),
                 _ => None,
             }
         }
---
+++ src/year_2023/day_5/mod.rs
@@ -0,0 +1,1 @@
+pub mod planting_seeds;
