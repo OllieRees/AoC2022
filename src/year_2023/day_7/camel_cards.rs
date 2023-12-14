ac8f37301eccf00db5e73b614aa74b6616a9c929
Ollie Rees <ollie.rees@me.com>
2023-12-14 18:06:59 +0000

    Part 2 completed

--- src/year_2023/day_7/camel_cards.rs
+++ src/year_2023/day_7/camel_cards.rs
@@ -1,26 +1,19 @@
-use std::{cmp, str, collections};
+use std::{cmp, str, collections::{self, HashMap}};
 
 use itertools::Itertools;
 
 use crate::ParseInputError;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-enum Card {
-    Ace,
-    King,
-    Queen, 
-    Jack, 
-    Tens,
-    Digit (u8),
-    Joker, 
-}
+enum Card { Ace, King, Queen, Jack, Tens, Digit (u8), Joker }
+type Cards = [Card; 5];
 impl Card {
-    fn try_from(value: char, j_card: Card) -> Result<Self, ParseInputError> {
+    fn try_parse(value: char, j_card: Card) -> Result<Self, ParseInputError> {
         match value.to_ascii_uppercase() {
             'A' => Ok(Card::Ace),
             'K' => Ok(Card::King),
             'Q' => Ok(Card::Queen),
-            'J' => Ok(j_card), // could be jack
+            'J' => Ok(j_card),
             'T' => Ok(Card::Tens),
             _ => {
                 match value.to_digit(10) {
@@ -33,15 +26,7 @@ impl Card {
 }
 impl Into<u8> for Card {
     fn into(self) -> u8 {
-        match self {
-            Card::Joker => 1,
-            Card::Digit(x) => x,
-            Card::Tens => 10,
-            Card::Jack => 11,
-            Card::Queen => 12,
-            Card::King => 13,
-            Card::Ace => 14,
-        }
+        match self { Card::Joker => 1, Card::Digit(x) => x, Card::Tens => 10, Card::Jack => 11, Card::Queen => 12, Card::King => 13, Card::Ace => 14, } 
     }
 }
 impl Ord for Card {
@@ -57,40 +42,32 @@ impl PartialOrd for Card {
     }
 }
 
-
-
-type Cards = [Card; 5];
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Type {
-    FiveOfAKind = 7,
-    FourOfAKind = 6,
-    FullHouse = 5,
-    ThreeOfAKind = 4,
-    TwoPair = 3,
-    OnePair = 2,
-    HighCard = 1,
-}  
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)] 
+enum Type { FiveOfAKind = 7, FourOfAKind = 6, FullHouse = 5, ThreeOfAKind = 4, TwoPair = 3, OnePair = 2, HighCard = 1 }  
 impl Type {
     fn determine_type(cards: Cards) -> Self {
-        let count_frequency = |card: Card| -> usize { cards.into_iter().filter(|x: &Card| *x == card).count() };
-        let frequencies: Vec<usize> = collections::HashSet::from(cards).into_iter().map(count_frequency).sorted().collect::<Vec<usize>>();  
-        let mut hand_type: Self = match frequencies[..] {
-            [5] => Self::FiveOfAKind,           
-            [1, 4] => Self::FourOfAKind,        
-            [2, 3] => Self::FullHouse,          
-            [1, 1, 3] => Self::ThreeOfAKind,    
-            [1, 2, 2] => Self::TwoPair,         
-            [1, 1, 1, 2] => Self::OnePair,      
-            _ => Self::HighCard,                
+        let mut frequencies: collections::HashMap<usize, usize> = HashMap::from_iter([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]);
+        for card in collections::HashSet::from(cards).into_iter().filter(|card: &Card| *card != Card::Joker) {
+            *frequencies.entry(cards.into_iter().filter(|x: &Card| *x == card).count()).or_insert(0) += 1;
+        }
+        let mut hand_type: Self = match frequencies {
+            _ if *frequencies.get(&5).unwrap() == 1 => Self::FiveOfAKind,
+            _ if *frequencies.get(&4).unwrap() == 1 => Self::FourOfAKind,
+            _ if *frequencies.get(&3).unwrap() == 1 && *frequencies.get(&2).unwrap() == 1 => Self::FullHouse,
+            _ if *frequencies.get(&3).unwrap() == 1 => Self::ThreeOfAKind,
+            _ if *frequencies.get(&2).unwrap() == 2 => Self::TwoPair,
+            _ if *frequencies.get(&2).unwrap() == 1 => Self::OnePair,
+            _ => Self::HighCard
         };
-        for _ in 0..count_frequency(Card::Joker) {
+
+        for _ in 0..cards.into_iter().filter(|x: &Card| *x == Card::Joker).count() {
             hand_type = match hand_type {
                 Self::HighCard => Self::OnePair,
                 Self::OnePair => Self::ThreeOfAKind,
                 Self::TwoPair => Self::FullHouse,
                 Self::ThreeOfAKind => Self::FourOfAKind,
                 Self::FullHouse => Self::FourOfAKind,
+                Self::FourOfAKind => Self::FiveOfAKind,
                 _ => hand_type,
             }
         }
@@ -110,7 +87,7 @@ impl Hand {
 
     fn parse(s: &str, j_card: Card) -> Result<Self, ParseInputError> {
         let (hand, bid) = s.split_whitespace().collect_tuple::<(&str, &str)>().ok_or(ParseInputError {details: "Hand and bid are not delimited by ':'".to_string()})?;
-        let value: Cards = hand.chars().map(|c| Card::try_from(c, j_card)).collect::<Result<Vec<Card>, _>>()?.try_into().map_err(|_| ParseInputError{ details: "Hand does not consist of exactly 5 cards".to_string() })?;
+        let value: Cards = hand.chars().map(|c| Card::try_parse(c, j_card)).collect::<Result<Vec<Card>, _>>()?.try_into().map_err(|_| ParseInputError{ details: "Hand does not consist of exactly 5 cards".to_string() })?;
         let bid = bid.parse::<u32>()?;
         Ok(Hand { value, bid })
     }
@@ -156,13 +133,19 @@ mod camel_cards {
 
     #[test]
     fn test_parsing_card() {
-        assert_eq!(Card::try_from('A', Card::Joker), Ok(Card::Ace));
-        assert_eq!(Card::try_from('2', Card::Joker), Ok(Card::Digit(2)));
+        assert_eq!(Card::try_parse('A', Card::Joker), Ok(Card::Ace));
+        assert_eq!(Card::try_parse('2', Card::Joker), Ok(Card::Digit(2)));
+    }
+
+    #[test]
+    fn test_parsing_j_card() {
+        assert_eq!(Card::try_parse('J', Card::Jack), Ok(Card::Jack));
+        assert_eq!(Card::try_parse('J', Card::Joker), Ok(Card::Joker));
     }
 
     #[test]
     fn test_parsing_card_with_unmatched_char() {
-        assert!(Card::try_from('I', Card::Joker).is_err());
+        assert!(Card::try_parse('I', Card::Joker).is_err());
     }
 
     #[test]
@@ -193,15 +176,25 @@ mod camel_cards {
     }
 
     #[test]
-    fn test_determine_type_one_joker() {
+    fn test_determine_type_joker() {
+        let cards: Cards = [Card::Digit(5), Card::Digit(5), Card::Digit(5), Card::Joker, Card::Digit(5)];
+        assert_eq!(Type::determine_type(cards), Type::FiveOfAKind);
         let cards: Cards = [Card::Tens, Card::Digit(5), Card::Digit(5), Card::Joker, Card::Digit(5)];
         assert_eq!(Type::determine_type(cards), Type::FourOfAKind);
+        let cards: Cards = [Card::King, Card::Tens, Card::Joker, Card::Joker, Card::Tens];
+        assert_eq!(Type::determine_type(cards), Type::FourOfAKind);
+        let cards: Cards = [Card::King, Card::Joker, Card::Joker, Card::Digit(7), Card::Tens];
+        assert_eq!(Type::determine_type(cards), Type::ThreeOfAKind);
+        let cards: Cards = [Card::Digit(7), Card::Tens, Card::Joker, Card::Digit(7), Card::Tens];
+        assert_eq!(Type::determine_type(cards), Type::FullHouse);
+        let cards: Cards = [Card::King, Card::Digit(3), Card::Joker, Card::Digit(7), Card::Tens];
+        assert_eq!(Type::determine_type(cards), Type::OnePair);
     }
 
     #[test]
-    fn test_determine_type_two_joker() {
-        let cards: Cards = [Card::King, Card::Tens, Card::Joker, Card::Joker, Card::Tens];
-        assert_eq!(Type::determine_type(cards), Type::FourOfAKind);
+    fn test_five_jokers() {
+        let cards: Cards = [Card::Joker, Card::Joker, Card::Joker, Card::Joker, Card::Joker];
+        assert_eq!(Type::determine_type(cards), Type::FiveOfAKind);
     }
 
     #[test]
@@ -214,6 +207,13 @@ mod camel_cards {
     fn test_compare_hand_equal_type() {
         let hand_a = Hand { value: [Card::Tens, Card::Digit(5), Card::Digit(5), Card::Jack, Card::Digit(5)], bid: 684 };
         let hand_b = Hand { value: [Card::Queen, Card::Queen, Card::Queen, Card::Jack, Card::Ace], bid: 483 };
+        assert!(hand_b > hand_a);
+    }
+    
+    #[test]
+    fn test_compare_almost_same_hand() {
+        let hand_a = Hand { value: [Card::Tens, Card::Digit(7), Card::Digit(7), Card::Jack, Card::Digit(5)], bid: 0 };
+        let hand_b = Hand { value: [Card::Tens, Card::Digit(7), Card::Digit(7), Card::Jack, Card::Digit(6)], bid: 0 };
         assert!(hand_b > hand_a);
     }
 
