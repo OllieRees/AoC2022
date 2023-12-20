f772e83a1420a18729e954f7ef0ed9a1cfd1b0d6
Ollie Rees <ollie.rees@me.com>
2023-12-20 18:09:05 +0000

    Remove row cloning

--- src/year_2023/day_9/mirage_maintenance.rs
+++ src/year_2023/day_9/mirage_maintenance.rs
@@ -4,27 +4,27 @@ fn parse_history(line: String) -> Result<Vec<i32>, ParseInputError> {
     line.split_whitespace().map(|x| x.parse::<i32>().map_err(|_| ParseInputError {details: format!("Can't parse {x} to i32")} )).collect()
 }
 
-fn predict_next_value(history: Vec<i32>) -> i32 {
+fn predict_next_value(history: &Vec<i32>) -> i32 {
     if history.iter().all(|x| *x == 0) {
         0
     } else {
-        history.last().unwrap_or(&0) + predict_next_value(history.windows(2).map(|adjs| adjs[1] - adjs[0]).collect())
+        history.last().unwrap_or(&0) + predict_next_value(&history.windows(2).map(|adjs| adjs[1] - adjs[0]).collect())
     }
 }
 
-fn predict_previous_value(history: Vec<i32>) -> i32 {
+fn predict_previous_value(history: &Vec<i32>) -> i32 {
     if history.iter().all(|x| *x == 0) {
         0
     } else {
-        history.first().unwrap_or(&0) - predict_previous_value(history.windows(2).map(|adjs| adjs[1] - adjs[0]).collect())
+        history.first().unwrap_or(&0) - predict_previous_value(&history.windows(2).map(|adjs| adjs[1] - adjs[0]).collect())
     }
 }
 
 pub fn solve(lines: Vec<String>) {
     if let Ok(history) = lines.into_iter().map(parse_history).collect::<Result<Vec<Vec<i32>>, _>>() {
-        let next_value_sum: i32 = history.iter().map(|row| predict_next_value(row.clone())).sum();
+        let next_value_sum: i32 = history.iter().map(|row| predict_next_value(row)).sum();
         println!("Sum of Next Values {next_value_sum}");
-        let prev_value_sum: i32 = history.iter().map(|row| predict_previous_value(row.clone())).sum();
+        let prev_value_sum: i32 = history.iter().map(|row| predict_previous_value(row)).sum();
         println!("Sum of Previous Values {prev_value_sum}");
     }
 }
@@ -59,17 +59,17 @@ mod mirage_maintenance {
 
     #[test]
     fn test_predict_next_value() {
-        let history: Vec<i32> = vec![0, 3, 6, 9, 12, 15];
+        let history: &Vec<i32> = &vec![0, 3, 6, 9, 12, 15];
         assert_eq!(predict_next_value(history), 18);
-        let history: Vec<i32> = vec![1, 3, 6, 10, 15, 21];
+        let history: &Vec<i32> = &vec![1, 3, 6, 10, 15, 21];
         assert_eq!(predict_next_value(history), 28);
-        let history: Vec<i32> = vec![10, 13, 16, 21, 30, 45];
+        let history: &Vec<i32> = &vec![10, 13, 16, 21, 30, 45];
         assert_eq!(predict_next_value(history), 68);
     }
 
     #[test]
     fn test_predict_previous_value() {
-        let history: Vec<i32> = vec![10, 13, 16, 21, 30, 45];
+        let history: &Vec<i32> = &vec![10, 13, 16, 21, 30, 45];
         assert_eq!(predict_previous_value(history), 5);
     }
 }
