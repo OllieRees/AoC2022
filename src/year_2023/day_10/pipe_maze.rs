5df93ec244d8318804891843172e1168e782e1d5
Ollie Rees <ollie.rees@me.com>
2024-04-13 23:22:11 +0100

    Tests and wiping the domain models clean

--- src/year_2023/day_10/pipe_maze.rs
+++ src/year_2023/day_10/pipe_maze.rs
@@ -1,4 +1,4 @@
-use std::collections;
+use std::{collections};
 
 use crate::ParseInputError;
 
@@ -23,61 +23,56 @@ impl TryFrom<char> for Pipe {
         }
     }
 }
-impl Pipe {
-    pub fn connecting_positions(&self, grid_position: Position) -> Option<(Position, Position)> {
-        match self {
-            Self::Vertical => Some(((grid_position.0 + 1, grid_position.1), (grid_position.0 - 1, grid_position.1))),
-            Self::Horizontal => Some(((grid_position.0, grid_position.1 + 1), (grid_position.0, grid_position.1 - 1))),
-            Self::NorthEast => Some(((grid_position.0 + 1, grid_position.1), (grid_position.0, grid_position.1 + 1))),
-            Self::NorthWest => Some(((grid_position.0 + 1, grid_position.1), (grid_position.0, grid_position.1 - 1))),
-            Self::SouthEast => Some(((grid_position.0 - 1, grid_position.1), (grid_position.0, grid_position.1 + 1))),
-            Self::SouthWest => Some(((grid_position.0 - 1, grid_position.1), (grid_position.0, grid_position.1 - 1))),
-            Self::Ground => Some((grid_position, grid_position)),
-            Self::Start => None,
-
-        }
-    }
-
-    pub fn next_position(&self, prior_position: Position, grid_position: Position) -> Option<Position> {
-        let connecting_positions = self.connecting_positions(grid_position);
-        match connecting_positions {
-            Some((position_1, position_2)) => if position_1 == prior_position {Some(position_2)} else {Some(position_1)},            
-            None => None,
-        }
-    }
-}
 
 #[derive(Debug)]
 struct Grid(collections::HashMap<Position, Pipe>);
+
 impl Grid {
     pub fn new(lines: Vec<String>) -> Result<Self, ParseInputError> {       
-        let parse_line = |(row_index, line): (usize, String)| -> Result<Vec<(Position, Pipe)>, ParseInputError> { 
-            line.chars().enumerate().map(|(char_index, c)| {
-                match Pipe::try_from(c) {
-                    Ok(pipe) => Ok(((row_index, char_index), pipe)),
-                    Err(e) => Err(e),
-                }
-            }).collect() 
-        };          
-        Ok(Grid(lines.into_iter().enumerate().map(parse_line).flatten().flatten().collect()))
+        Err(ParseInputError{details: "".to_string()})
     }
-
-    pub fn get_start_position(&self) -> Position {
-        self.0.iter().find_map(|(pos, pipe)| if *pipe == Pipe::Start {Some(*pos)} else {None}).unwrap()
-    }
-
-    pub fn get_cycle_positions(&self) -> Vec<Position> {
-        // https://en.wikipedia.org/wiki/Cycle_detection#Algorithms
+    
+    pub fn get_cycle_from_start() -> Vec<Position> {
         Vec::new()
     }
 
-    pub fn get_longest_distance(&self) -> usize {
-        self.get_cycle_positions().len() / 2 
+    pub fn get_adjancet_pipes_from_position(pos: &Position) -> Vec<Position> {
+        Vec::new()
+    }
+
+    fn get_position_from_start() -> Position {
+        (0, 0)
     }
 }
 
 pub fn solve(lines: Vec<String>) {
-    if let Ok(grid) = Grid::new(lines) {
-        println!("Cycle Positions: {:?}", grid.get_cycle_positions());
+
+}
+
+#[cfg(test)]
+mod pipe_maze {
+    use crate::year_2023::day_10::pipe_maze::*;
+
+    const GRID: [&str; 5] = ["..F7.", ".FJ|.", "SJ. L7", "|F--J", "LJ..."];
+
+    fn parse_grid() -> Grid {
+        Grid::new(GRID.into_iter().map(|s: &str| s.to_string()).collect::<Vec<String>>()).unwrap()
+    }
+
+    #[test]
+    fn parse_all_pipe_types_successfully() {
+        assert_eq!(Pipe::try_from('S'), Ok(Pipe::Start));
+        assert_eq!(Pipe::try_from('|'), Ok(Pipe::Vertical));
+        assert_eq!(Pipe::try_from('-'), Ok(Pipe::Horizontal));
+        assert_eq!(Pipe::try_from('L'), Ok(Pipe::NorthEast));
+        assert_eq!(Pipe::try_from('J'), Ok(Pipe::NorthWest));
+        assert_eq!(Pipe::try_from('F'), Ok(Pipe::SouthEast));
+        assert_eq!(Pipe::try_from('7'), Ok(Pipe::SouthWest));
+        assert_eq!(Pipe::try_from('.'), Ok(Pipe::Ground));
+    }
+
+    #[test]
+    fn parse_pipe_types_with_invalid_char() {
+        assert_eq!(Pipe::try_from('X'), Err(ParseInputError {details: "Could not parse X to a pipe type".to_string()}));
     }
 }
