c25373cfd32d92f61e7051d01a6b2c8d00c286cd
Ollie Rees <ollie.rees@me.com>
2024-04-21 22:06:44 +0100

    Using petgraph to represent directed graph

--- src/year_2023/day_10/pipe_maze.rs
+++ src/year_2023/day_10/pipe_maze.rs
@@ -1,10 +1,10 @@
-use std::collections::{HashMap, VecDeque};
-use itertools::Itertools;
 use crate::ParseInputError;
+use petgraph::graphmap::DiGraphMap;
+
 
 type Position = (usize, usize);
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum Pipe { Start, Vertical, Horizontal, NorthEast, NorthWest, SouthEast, SouthWest, Ground }
 impl TryFrom<char> for Pipe {
     type Error = ParseInputError;
@@ -47,51 +47,71 @@ impl Pipe {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Grid(HashMap<Position, Pipe>);
 
-impl Grid {
-    pub fn new(lines: Vec<String>) -> Result<Self, ParseInputError> {       
-        let parse_row = |(row_index, line): (usize, String)| -> Result<Vec<(Position, Pipe)>, ParseInputError> { 
-            line.chars().enumerate().map(|(char_index, c)| Ok(((row_index, char_index), Pipe::try_from(c)?))).collect() 
-        };
-        Ok(Grid(lines.into_iter().enumerate().map(parse_row).collect::<Result<Vec<Vec<(Position, Pipe)>>, _>>()?.into_iter().flatten().collect()))
-    }
-    
-    pub fn get_cycle_from_start(&self) -> Vec<Position> {
-        // https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm#Stack_invariant
-        // Use a stack to find the cycle
-        let start_pos = self.get_position_from_start();
-        let mut dfs_stack: Vec<Position> = vec![start_pos];
-        while let Some(node) = dfs_stack.pop() {
-            if node == start_pos {
-                return dfs_stack
-            }
-            self.get_connected_positions(&node).into_iter().for_each(|node: (usize, usize)| dfs_stack.push(node));
-        }
-        Vec::new()
-    }
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+struct Node {
+    pos: Position,
+    pipe: Pipe
+}
 
-    pub fn get_connected_positions(&self, pos: &Position) -> Vec<Position> {
-        self.0.get(pos).unwrap_or(&Pipe::Ground).get_connected_positions(*pos).into_iter().filter(|pos| self.0.contains_key(pos)).collect()
-    }
-
-    fn get_position_from_start(&self) -> Position {
-        *self.0.iter().find_or_first(|(_, pipe)| **pipe==Pipe::Start).unwrap().0
+impl Node {
+    fn new(pos: Position, pipe_letter: char) -> Result<Self, ParseInputError> {
+        Ok(Node {pos, pipe: Pipe::try_from(pipe_letter)?})
     }
 }
 
 
-type DirectedGraph = HashMap<Position, Vec<Position>>;
-impl Into<DirectedGraph> for Grid { 
-    fn into(self) -> DirectedGraph {
-        self.0.iter().map(|(pos, _)| (*pos, self.get_connected_positions(&pos))).collect()
+#[derive(Debug, Clone)]
+struct PipeMaze(DiGraphMap<Node, ()>);
+
+impl TryFrom<Vec<String>> for PipeMaze {
+    type Error = ParseInputError;
+
+    fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
+        let mut grid: DiGraphMap<Node, ()> = DiGraphMap::new();
+
+        let nodes: Vec<Node> = value.into_iter().enumerate().map(|(row_n, row): (usize, String)| 
+            row.chars().enumerate().map(|(column_n, pipe_letter): (usize, char)| 
+                Node::new((row_n, column_n), pipe_letter)
+            ).collect()
+        ).collect::<Result<Vec<Vec<Node>>, ParseInputError>>()?.into_iter().flatten().collect();
+        
+        let get_neighbours = |node: &Node| -> Vec<&Node> {
+            nodes.iter().filter(|neighbour| node.pipe.get_connected_positions(node.pos).contains(&neighbour.pos)).collect()
+        };
+        
+        // populate graph
+        for node in nodes.iter() {
+            grid.add_node(*node);
+            for neighbour in  get_neighbours(node){
+                grid.add_edge(*node, *neighbour, ());
+            }
+        }
+        Ok(PipeMaze(grid))
+    }   
+}
+
+impl PipeMaze {
+    pub fn get_cycle_from_start(&self) -> Vec<Node> {
+        // https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm#Stack_invariant
+        // Use a stack to find the cycle
+        Vec::new()
+    }
+
+    pub fn get_connected_positions(&self, pos: &Position) -> Vec<Position> {
+        Vec::new()
+        // self.0.get(pos).unwrap_or(&Pipe::Ground).get_connected_positions(*pos).into_iter().filter(|pos| self.0.contains_key(pos)).collect()
+    }
+
+    fn get_position_from_start(&self) -> Position {
+        (0, 0)
+        // *self.0.iter().find_or_first(|(node, _)| node.pipe==Pipe::Start).unwrap().0.pos
     }
 }
 
 
 pub fn solve(lines: Vec<String>) {
-    if let Ok(grid) = Grid::new(lines) {
+    if let Ok(grid) = PipeMaze::try_from(lines) {
         println!("{}", grid.get_cycle_from_start().len() / 2 as usize);
     }
 }
@@ -125,36 +145,8 @@ mod pipe_maze {
     }
 
     #[test]
-    fn parse_grid_successfully() {
-        let grid: Grid = Grid::new(grid()).unwrap();
-        assert_eq!(grid.0.get(&(0, 0)), Some(&Pipe::Ground));
-        assert_eq!(grid.0.get(&(0, 2)), Some(&Pipe::SouthEast));
-        assert_eq!(grid.0.get(&(2, 0)), Some(&Pipe::Start));
-        assert_eq!(grid.0.get(&(1, 3)), Some(&Pipe::Vertical));
-        assert_eq!(grid.0.get(&(3, 2)), Some(&Pipe::Horizontal));
-    }
-
-    #[test]
-    fn parse_grid_with_one_bad_row() {
-        assert!(Grid::new(vec!["..F7.".to_string(), ".FX|.".to_string(), "SJ.L7".to_string()]).is_err());
-    }
-
-    #[test]
-    fn parse_grid_into_directed_graph() {
-        let grid: Grid = Grid::new(grid()).unwrap();
-        let directed_graph_grid: DirectedGraph = grid.into();
-        assert_eq!(directed_graph_grid.get(&(0, 2)).unwrap(), &vec![(0, 3), (1, 2)]);
-    }
-
-    #[test]
-    fn get_position_of_start() {
-        let grid: Grid = Grid::new(grid()).unwrap();
-        assert_eq!(grid.get_position_from_start(), (2, 0));
-    }
-
-    #[test]
     fn get_all_pipe_connected_positions_successfully() {
-        assert_eq!(Pipe::Start.get_connected_positions((0, 0)), vec![(1, 0), (0, 1)]);
+        assert_eq!(Pipe::Start.get_connected_positions((1, 1)), vec![(0, 1), (2, 1), (1, 0), (1, 2)]);
         assert_eq!(Pipe::Vertical.get_connected_positions((1, 1)), vec![(1, 0), (1, 2)]);
         assert_eq!(Pipe::Horizontal.get_connected_positions((1, 1)), vec![(0, 1), (2, 1)]);
         assert_eq!(Pipe::NorthEast.get_connected_positions((1, 1)), vec![(1, 0), (2, 1)]);
@@ -165,7 +157,7 @@ mod pipe_maze {
     }
 
     #[test]
-    fn get_all_pipe_connected_positions_from_origin() {
+    fn get_all_pipe_connected_positions_from_corner() {
         assert_eq!(Pipe::Start.get_connected_positions((0, 0)), vec![(1, 0), (0, 1)]);
         assert_eq!(Pipe::Vertical.get_connected_positions((0, 0)), vec![(0, 1)]);
         assert_eq!(Pipe::Horizontal.get_connected_positions((0, 0)), vec![(1, 0)]);
@@ -177,24 +169,68 @@ mod pipe_maze {
     }
 
     #[test]
-    fn get_connected_pipes_from_grid() {
-        let grid: Grid = Grid::new(grid()).unwrap();
-        assert_eq!(grid.get_connected_positions(&(0, 2)), vec![(0, 3), (1, 2)]);
-        assert_eq!(grid.get_connected_positions(&(2, 0)), vec![(1, 0), (3, 0), (2, 1)]);
-        assert_eq!(grid.get_connected_positions(&(4, 4)), vec![]);
-        assert_eq!(grid.get_connected_positions(&(5, 5)), vec![]);
+    fn new_node() {
+        assert_eq!(Node::new((0, 0), 'S').unwrap(), Node {pos: (0, 0), pipe: Pipe::Start});
     }
 
     #[test]
-    fn get_cycle_from_start_practice_grid() {
-        let grid: Grid = Grid::new(grid()).unwrap();
-        assert_eq!(
-            grid.get_cycle_from_start(), 
-            vec![
-                (2, 0), (2, 1), (1, 1), (1, 2), (0, 2), (0, 3), (1, 3), (2, 3), 
-                (2, 4), 
-                (3, 4), (3, 3), (3, 2), (3, 1) , (4, 1), (4, 0), (3, 0), (2, 0)
-            ]
-        );
+    fn new_node_err() {
+        assert!(Node::new((0, 0), 'X').is_err());
     }
+
+
+    #[test]
+    fn parse_grid_nodes_successfully() {
+        let grid_3x3: PipeMaze = PipeMaze::try_from(vec![String::from("S-7"), String::from("|.|"), String::from("L-J")]).unwrap();
+        assert!(grid_3x3.0.contains_node(Node {pos: (0, 0), pipe: Pipe::Start}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (0, 1), pipe: Pipe::Horizontal}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (0, 2), pipe: Pipe::SouthWest}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (1, 0), pipe: Pipe::Vertical}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (1, 1), pipe: Pipe::Ground}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (1, 2), pipe: Pipe::Vertical}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (2, 0), pipe: Pipe::NorthEast}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (2, 1), pipe: Pipe::Horizontal}));
+        assert!(grid_3x3.0.contains_node(Node {pos: (2, 2), pipe: Pipe::NorthWest}));
+    }
+
+    #[test]
+    fn parse_grid_edges_successfully() {
+        let grid: PipeMaze = PipeMaze::try_from(vec![String::from("S-7"), String::from("|.|"), String::from("L-J")]).unwrap();
+        assert!(grid.0.edge_count() == 8);  
+        let grid: PipeMaze = PipeMaze::try_from(vec![String::from("S.7"), String::from("L.|"), String::from("..L")]).unwrap();
+        assert!(grid.0.edge_count() == 5);  
+    }
+
+    #[test]
+    fn parse_grid_with_one_bad_row() {
+        assert!(PipeMaze::try_from(vec!["..F7.".to_string(), ".FX|.".to_string(), "SJ.L7".to_string()]).is_err());
+    }
+
+    // #[test]
+    // fn get_position_of_start() {
+    //     let grid: Grid = Grid::try_from(grid()).unwrap();
+    //     assert_eq!(grid.get_position_from_start(), (2, 0));
+    // }
+
+    // #[test]
+    // fn get_connected_pipes_from_grid() {
+    //     let grid: Grid = Grid::from_lines(grid()).unwrap();
+    //     assert_eq!(grid.get_connected_positions(&(0, 2)), vec![(0, 3), (1, 2)]);
+    //     assert_eq!(grid.get_connected_positions(&(2, 0)), vec![(1, 0), (3, 0), (2, 1)]);
+    //     assert_eq!(grid.get_connected_positions(&(4, 4)), vec![]);
+    //     assert_eq!(grid.get_connected_positions(&(5, 5)), vec![]);
+    // }
+
+    // #[test]
+    // fn get_cycle_from_start_practice_grid() {
+    //     let grid: Grid = Grid::from_lines(grid()).unwrap();
+    //     assert_eq!(
+    //         grid.get_cycle_from_start(), 
+    //         vec![
+    //             (2, 0), (2, 1), (1, 1), (1, 2), (0, 2), (0, 3), (1, 3), (2, 3), 
+    //             (2, 4), 
+    //             (3, 4), (3, 3), (3, 2), (3, 1) , (4, 1), (4, 0), (3, 0), (2, 0)
+    //         ]
+    //     );
+    // }
  }
