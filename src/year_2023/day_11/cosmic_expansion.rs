8790a0ad33fcca95ff3fc5b40396876848ba2b2d
Ollie Rees <oliver.x.rees@gmail.com>
2024-10-02 15:55:07 +0100

    Adding functionality to get row and col indices that are expandable

--- src/year_2023/day_11/cosmic_expansion.rs
+++ src/year_2023/day_11/cosmic_expansion.rs
@@ -6,15 +6,30 @@ struct Galaxy {
 }
 
 struct Image {
+    size: (usize, usize),
     galaxies: HashSet<Galaxy>
+}
+
+impl Image {
+    pub fn expandable_rows(&self) -> impl Iterator<Item=usize> + '_ {
+        let range: HashSet<usize> = HashSet::from_iter(0..self.size.0);
+        let galaxy_rows: HashSet<usize> = self.galaxies.iter().map(|galaxies| galaxies.coord.0).collect();
+        (&range - &galaxy_rows).into_iter()
+    }
+    pub fn expandable_columns(&self) -> impl Iterator<Item=usize> + '_ {
+        let range: HashSet<usize> = HashSet::from_iter(0..self.size.1);
+        let galaxy_rows: HashSet<usize> = self.galaxies.iter().map(|galaxies| galaxies.coord.1).collect();
+        (&range - &galaxy_rows).into_iter()
+    }
 }
 
 impl From<Vec<String>> for Image {
     fn from(value: Vec<String>) -> Self {
+        let size = (value.len(), value.get(0).unwrap_or(&"".to_string()).len());
         let galaxies: HashSet<Galaxy> = value.into_iter().enumerate().map(|(row_n, row)| {
                 row.match_indices('#').map(|(col_n, _)| Galaxy {coord: (row_n, col_n)}).collect::<HashSet<Galaxy>>()
             }).flatten().collect();
-        Image {galaxies}
+        Image {size, galaxies}
     }
 }
 
@@ -24,10 +39,12 @@ pub fn solve(lines: Vec<String>) {
 
 #[cfg(test)]
 mod test_cosmic_expansion {
-    use super::{Galaxy, Image};
+    use itertools::Itertools;
+
+    use super::{Galaxy, Image, HashSet};
 
     #[test]
-    fn test_capture_image() {
+    fn capture_image() {
         let image: Image = Image::from(vec![
             "...#......".to_string(),
             ".......#..".to_string(),
@@ -50,5 +67,46 @@ mod test_cosmic_expansion {
         assert!(image.galaxies.contains(&Galaxy{coord: (8, 7)}));
         assert!(image.galaxies.contains(&Galaxy{coord: (9, 0)}));
         assert!(image.galaxies.contains(&Galaxy{coord: (9, 4)}));
+    }
+
+    #[test]
+    fn capture_empty_image() {
+        let image: Image = Image::from(vec![]);
+        assert_eq!(image.size, (0, 0));
+        assert_eq!(image.galaxies, HashSet::new());
+    }
+
+    #[test]
+    fn expandable_rows() {
+         let image: Image = Image::from(vec![
+            "...#......".to_string(),
+            ".......#..".to_string(),
+            "#.........".to_string(),
+            "..........".to_string(),
+            "......#...".to_string(),
+            ".#........".to_string(),
+            ".........#".to_string(),
+            "..........".to_string(),
+            ".......#..".to_string(),
+            "#...#.....".to_string(),
+        ]);
+        assert_eq!(image.expandable_rows().sorted().collect::<Vec<usize>>(), vec![3,7]);
+    }
+
+    #[test]
+    fn expandable_cols() {
+         let image: Image = Image::from(vec![
+            "...#......".to_string(),
+            ".......#..".to_string(),
+            "#.........".to_string(),
+            "..........".to_string(),
+            "......#...".to_string(),
+            ".#........".to_string(),
+            ".........#".to_string(),
+            "..........".to_string(),
+            ".......#..".to_string(),
+            "#...#.....".to_string(),
+        ]);
+        assert_eq!(image.expandable_columns().sorted().collect::<Vec<usize>>(), vec![2,5,8]);
     }
 }
